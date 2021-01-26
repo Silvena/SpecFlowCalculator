20610ad656c4857aab05a0bd953bb628bec054ea
sshtereva <silvena.shtereva@endava.com>
2021-01-26 13:29:31 +0200

    change some adds

---
+++ SpecFlowCalculator/Claculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SpecFlowCalculator
+{
+    public class Claculator
+    {  
+        public int FirstNumber { get; set; }
+        public int SecontNumber { get; set; }
+        public int Add()
+        { 
+         throw new NotImplementedException();
+        }
+   
+    }
+}
